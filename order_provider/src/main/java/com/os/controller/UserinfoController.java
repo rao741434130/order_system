416a7c5dcff10ed86d07b9b0b757f9581455f9bb
rao741434130 <741434130@qq.com>
2020-05-08 15:37:46 +0800

    修改一些配置文件

--- order_provider/src/main/java/com/os/controller/UserinfoController.java
+++ order_provider/src/main/java/com/os/controller/UserinfoController.java
@@ -4,16 +4,13 @@ import com.os.entity.Userinfo;
 import com.os.service.UserinfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author R
  * @date 2020/5/7 - 21:11
  */
-@Controller
+@RestController
 public class UserinfoController {
 
     @Autowired
@@ -21,7 +18,6 @@ public class UserinfoController {
 
 
     @PostMapping("login")
-    @ResponseBody
     public Userinfo login(@RequestBody Userinfo userinfo) {
         userinfo= userinfoService.loginUser(userinfo);
         System.out.println(userinfo);
